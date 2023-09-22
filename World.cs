aa9c485c5b15197acc55bf7fdbda3ac6321fef90
Tijmen <tijmenvanegmond@gmail.com>
2023-09-22 11:19:45 +0200

    more setup

--- World.cs
+++ World.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class World : Node
 {
+ 	[Export]
+	public PackedScene ChunkScene { get; set; }
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +14,20 @@ public partial class World : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void spawnChunk(int size = 3)
+	{
+		Node3D chunk = ChunkScene.Instantiate<Node3D>();
+		
+		var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+		// And give it a random offset.
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		Vector3 playerPosition = GetNode<Player>("Player").Position;
+		mob.Initialize(mobSpawnLocation.Position, playerPosition);
+
+		// Spawn the mob by adding it to the Main scene.
+		AddChild(mob);
 	}
 }
--- player.gd
+++ player.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
+@export var jump_power = 10
+
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
@@ -31,6 +33,8 @@ func _physics_process(delta):
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	if is_on_floor() && Input.is_action_pressed("dodge"):
+		target_velocity.y = jump_power
 
 	# Moving the Character
 	velocity = target_velocity
