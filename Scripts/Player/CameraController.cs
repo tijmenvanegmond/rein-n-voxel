0e36c0005dc8ce02db300aec38ae55f8e1c233af
Tijmen <tijmenvanegmond@gmail.com>
2023-10-17 23:30:03 +0200

    cs moevment scripts

---
+++ Scripts/Player/CameraController.cs
@@ -0,0 +1,36 @@
+using Godot;
+
+public partial class CameraController : Node3D
+{
+    [Export]
+    public CharacterBody3D playerNode { get; set; }
+
+    [Export]
+    public float MOVEMENT_SPEED { get; set; } = 5f;
+
+    public float ROTATION_SPEED { get; set; }
+
+    public override void _Ready()
+    {
+    }
+
+    public override void _Process(double delta)
+    {
+        var rotation_delta = 0f;
+        if (Input.IsActionPressed("rotate_right"))
+        {
+            rotation_delta += 1f;
+        }
+        if (Input.IsActionPressed("rotate_left"))
+        {
+
+            rotation_delta -= 1f;
+        }
+
+        RotateY(rotation_delta * ROTATION_SPEED * (float)delta);
+
+
+        Position = Position.Lerp(playerNode.Position, MOVEMENT_SPEED * (float)delta);
+
+    }
+}
---
+++ Scripts/Player/MovementController.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+
+public partial class MovementController : CharacterBody3D
+{
+	[Export]
+	public Node3D Pivot;
+	[Export]
+	public const float Speed = 5.0f;
+	[Export]
+	public const float JumpVelocity = 4.5f;
+	public Vector3 MovementDirection {get; protected set;}
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y -= gravity * (float)delta;
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+
+		MovementDirection = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		
+		if (MovementDirection != Vector3.Zero)
+		{
+			velocity.X = MovementDirection.X * Speed;
+			velocity.Z = MovementDirection.Z * Speed;
+
+			Pivot.LookAt(Position + MovementDirection);
+
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
